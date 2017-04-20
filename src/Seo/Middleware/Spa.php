cbf8c3e953497b7962c4397b41f8d402fe5efc7a
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-04-20 19:12:39 +0430

    Strunct project

---
+++ src/Seo/Middleware/Spa.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * SEO middleware
+ * 
+ * 
+ * @author maso <mostafa.barmshory@dpq.co.ir>
+ *        
+ */
+class Seo_Middleware_Spa
+{
+
+    
+    /**
+     * Check request to detect bot
+     * 
+     * @param Pluf_HTTP_Request $request
+     */
+    function process_request (&$request)
+    {
+        // در صورتی که درخواست مربوط به seo باشد
+        if (array_key_exists('_escaped_fragment_', $request->GET)) {
+            return $this->prerenderResponse($request);
+        }
+        return false;
+    }
+    
+    /**
+     * بر اساس تقاضا یک نتیجه مناسب برای جستجوی گوگل ایجاد می‌کند.
+     *
+     * @param Pluf_HTTP_Request $request
+     */
+    function prerenderResponse($request)
+    {
+        $context = new Pluf_Template_Context(array(
+                'tenant' => $request->tenant
+        ));
+        $tmpl = new Pluf_Template('seo.template');
+        $html = $tmpl->render($context);
+        return new Pluf_HTTP_Response($tmpl->render($context));
+    }
+    
+}
---
+++ src/Seo/SitemapLink.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+class Seo_SitemapLink extends Pluf_Model
+{
+
+    /**
+     *
+     * @see Pluf_Model::init()
+     */
+    function init ()
+    {
+        $this->_a['table'] = 'seo_sitemap_link';
+        $this->_a['cols'] = array(
+                'id' => array(
+                        'type' => 'Pluf_DB_Field_Sequence',
+                        'blank' => true,
+                        'verbose' => 'unique and no repreducable id fro reception'
+                ),
+                'title' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => true,
+                        'size' => 50
+                ),
+                'description' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => true,
+                        'size' => 200
+                ),
+                'loc' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => true,
+                        'size' => 50
+                ),
+                'lastmod' => array(
+                        'type' => 'Pluf_DB_Field_Datetime',
+                        'blank' => true,
+                        'verbose' => 'last modification date'
+                ),
+                'changefreq' => array(
+                        'type' => 'Pluf_DB_Field_Varchar',
+                        'blank' => false,
+                        'size' => 16
+                ),
+                'priority' => array(
+                        'type' => 'Pluf_DB_Field_Float',
+                        'blank' => true,
+                        'editable' => true,
+                        'readable' => true
+                ),
+                
+                'creation_dtime' => array(
+                        'type' => 'Pluf_DB_Field_Datetime',
+                        'blank' => true,
+                        'verbose' => 'creation date'
+                ),
+                'modif_dtime' => array(
+                        'type' => 'Pluf_DB_Field_Datetime',
+                        'blank' => true,
+                        'verbose' => 'modification date'
+                )
+        );
+    }
+}
+
