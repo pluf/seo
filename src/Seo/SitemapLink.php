881b167ad94bc04fa6442ffd93892a4a09fc06e1
maso <mostafa.barmshory@dpq.co.ir>
2017-12-11 10:01:30 +0330

    Remove automatic links from site map

--- src/Seo/SitemapLink.php
+++ src/Seo/SitemapLink.php
@@ -17,6 +17,53 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+/**
+ * # Properties
+ * 
+ * Main properites of a link is fully described here. The others are used
+ * in view and other part of the system.
+ * 
+ * ## loc
+ *
+ * URL of the page. This URL must begin with the protocol (such as http)
+ * and end with a trailing slash, if your web server requires it. This
+ * value must be less than 2,048 characters.
+ *
+ * ## lastmod
+ *
+ * The date of last modification of the file. This date should be in W3C
+ * Datetime format. This format allows you to omit the time portion, if
+ * desired, and use YYYY-MM-DD.
+ *
+ * ## changefreq
+ *
+ * How frequently the page is likely to change. This value provides general
+ * information to search engines and may not correlate exactly to how often
+ * they crawl the page. Valid values are:
+ *
+ * - always
+ * - hourly
+ * - daily
+ * - weekly
+ * - monthly
+ * - yearly
+ * - never
+ *
+ *
+ * The value "always" should be used to describe documents that change each time
+ * they are accessed. The value "never" should be used to describe archived URLs.
+ *
+ * ## priority
+ *
+ * The priority of this URL relative to other URLs on your site. Valid values
+ * range from 0.0 to 1.0. This value does not affect how your pages are
+ * ompared to pages on other sites—it only lets the search engines know
+ * which pages you deem most important for the crawlers.
+ * The default priority of a page is 0.5.
+ *
+ * @author Mostafa Barmshori<mostafa.barmshory@dpq.co.ir>
+ *        
+ */
 class Seo_SitemapLink extends Pluf_Model
 {
 
@@ -24,79 +71,77 @@ class Seo_SitemapLink extends Pluf_Model
      *
      * @see Pluf_Model::init()
      */
-    function init ()
+    function init()
     {
         $this->_a['table'] = 'seo_sitemap_link';
         $this->_a['cols'] = array(
-                'id' => array(
-                        'type' => 'Pluf_DB_Field_Sequence',
-                        'blank' => true,
-                        'verbose' => 'unique and no repreducable id fro reception'
-                ),
-                'title' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => true,
-                        'size' => 50,
-                        'editable' => true,
-                        'readable' => true
-                ),
-                'description' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => true,
-                        'size' => 200,
-                        'editable' => true,
-                        'readable' => true
-                ),
-                'loc' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => true,
-                        'size' => 50,
-                        'editable' => true,
-                        'readable' => true
-                ),
-                'lastmod' => array(
-                        'type' => 'Pluf_DB_Field_Datetime',
-                        'blank' => true,
-                        'verbose' => 'last modification date',
-                        'editable' => true,
-                        'readable' => true
-                ),
-                'changefreq' => array(
-                        'type' => 'Pluf_DB_Field_Varchar',
-                        'blank' => false,
-                        'size' => 16,
-                        'editable' => true,
-                        'readable' => true
-                ),
-                'priority' => array(
-                        'type' => 'Pluf_DB_Field_Float',
-                        'blank' => true,
-                        'editable' => true,
-                        'readable' => true
-                ),
-                
-                'creation_dtime' => array(
-                        'type' => 'Pluf_DB_Field_Datetime',
-                        'blank' => true,
-                        'verbose' => 'creation date',
-                        'editable' => false,
-                        'readable' => true
-                ),
-                'modif_dtime' => array(
-                        'type' => 'Pluf_DB_Field_Datetime',
-                        'blank' => true,
-                        'verbose' => 'modification date',
-                        'editable' => false,
-                        'readable' => true
-                )
+            'id' => array(
+                'type' => 'Pluf_DB_Field_Sequence',
+                'blank' => true,
+                'verbose' => 'unique and no repreducable id fro reception'
+            ),
+            'title' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => true,
+                'size' => 50,
+                'editable' => true,
+                'readable' => true
+            ),
+            'description' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => true,
+                'size' => 200,
+                'editable' => true,
+                'readable' => true
+            ),
+            'loc' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => true,
+                'size' => 50,
+                'editable' => true,
+                'readable' => true
+            ),
+            'lastmod' => array(
+                'type' => 'Pluf_DB_Field_Datetime',
+                'blank' => true,
+                'verbose' => 'last modification date',
+                'editable' => true,
+                'readable' => true
+            ),
+            'changefreq' => array(
+                'type' => 'Pluf_DB_Field_Varchar',
+                'blank' => false,
+                'size' => 16,
+                'editable' => true,
+                'readable' => true
+            ),
+            'priority' => array(
+                'type' => 'Pluf_DB_Field_Float',
+                'blank' => true,
+                'editable' => true,
+                'readable' => true
+            ),
+            'creation_dtime' => array(
+                'type' => 'Pluf_DB_Field_Datetime',
+                'blank' => true,
+                'verbose' => 'creation date',
+                'editable' => false,
+                'readable' => true
+            ),
+            'modif_dtime' => array(
+                'type' => 'Pluf_DB_Field_Datetime',
+                'blank' => true,
+                'verbose' => 'modification date',
+                'editable' => false,
+                'readable' => true
+            )
         );
     }
-    
-    
+
     /*
      * @see Pluf_Model::preSave()
      */
-    function preSave ($create = false)
+    function preSave($create = false)
     {
         if ($this->id == '') {
             $this->creation_dtime = gmdate('Y-m-d H:i:s');
--- src/Seo/Views/Sitemap.php
+++ src/Seo/Views/Sitemap.php
@@ -12,28 +12,22 @@ class Seo_Views_Sitemap
     /**
      * Generate sitemap
      *
-     * @param Pluf_HTTP_Request $request            
-     * @param array $match            
+     * @param Pluf_HTTP_Request $request
+     * @param array $match
      * @return Pluf_HTTP_Response
      */
-    public static function get ($request, $match)
+    public static function get($request, $match)
     {
-        // Spas
-        $sp = new Spa_SPA();
-        $spaList = $sp->getList();
-        
         // Links
         $link = new Seo_SitemapLink();
         $links = $link->getList();
         
         // Add link to SPAs of tenant
         $tmpl = new Pluf_Template('/sitemap.template');
-        $context = new Pluf_Template_Context(
-                array(
-                        'tenant' => $request->tenant,
-                        'spaList' => $spaList,
-                        'links' => $links
-                ));
+        $context = new Pluf_Template_Context(array(
+            'tenant' => $request->tenant,
+            'links' => $links
+        ));
         $mimetype = Pluf::f('mimetype', 'text/xml') . '; charset=utf-8';
         return new Pluf_HTTP_Response($tmpl->render($context), $mimetype);
     }
