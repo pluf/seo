7944ac371998b9a3ca9672b7af527bfe0c913409
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2018-11-09 00:09:55 +0330

    set expire_dtime as editable

--- src/Seo/Content.php
+++ src/Seo/Content.php
@@ -153,11 +153,12 @@ class Seo_Content extends Pluf_Model
             ),
             'expire_dtime' => array(
                 'type' => 'Pluf_DB_Field_Datetime',
+                'default' => null,
                 'blank' => true,
                 'is_null' => true,
                 'verbose' => 'expired',
                 'help_text' => 'content expiration time',
-                'editable' => false
+                'editable' => true
             )
         );
     }
